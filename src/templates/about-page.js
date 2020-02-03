1aef6edeac6583d5ffa68330b072be00a66cb8ea
Niel Spengler <niel@ltsystems.co.za>
2020-02-03 21:46:19 +0200

    Update about-page.js

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,38 +1,48 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import Content, { HTMLContent } from '../components/Content';
+import mainLogo from '../img/eloise.png';
 
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
+  const PageContent = contentComponent || Content;
 
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+    <section className='section section--gradient'>
+      <div className='container'>
+        <div className='columns'>
+          <div className='column is-10 is-offset-1'>
+            <div className='section'>
+              <h2 className='title is-size-3 has-text-weight-bold is-bold-light'>
                 {title}
               </h2>
-              <PageContent className="content" content={content} />
+              <table>
+                <tr>
+                  <td>
+                    <img src={mainLogo} alt='eloise' />
+                  </td>
+                  <td>
+                    <PageContent className='content' content={content} />
+                  </td>
+                </tr>
+              </table>
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 AboutPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
-  contentComponent: PropTypes.func,
-}
+  contentComponent: PropTypes.func
+};
 
 const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
 
   return (
     <Layout>
@@ -42,14 +52,14 @@ const AboutPage = ({ data }) => {
         content={post.html}
       />
     </Layout>
-  )
-}
+  );
+};
 
 AboutPage.propTypes = {
-  data: PropTypes.object.isRequired,
-}
+  data: PropTypes.object.isRequired
+};
 
-export default AboutPage
+export default AboutPage;
 
 export const aboutPageQuery = graphql`
   query AboutPage($id: String!) {
@@ -60,4 +70,4 @@ export const aboutPageQuery = graphql`
       }
     }
   }
-`
+`;
